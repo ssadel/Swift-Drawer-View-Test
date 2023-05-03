7f30d0452a0f18ea0d7ca76c4434455d7b59f592
Sidney Sadel <sidneysadel@gmail.com>
2023-05-03 13:31:52 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Sidney Sadel on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
